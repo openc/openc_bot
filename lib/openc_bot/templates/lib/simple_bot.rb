e3b4d36aaea5c7fb1ccc4a798e3a773e6ff1ac86
Seb Bacon <seb.bacon@gmail.com>
2014-01-07 10:18:42 +0000

    Better "simple bot" template

--- lib/openc_bot/templates/lib/simple_bot.rb
+++ lib/openc_bot/templates/lib/simple_bot.rb
@@ -7,7 +7,7 @@ require 'simple_openc_bot'
 class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
   # Fields you define here will be persisted to a local database when
   # 'fetch_records' (see below) is run.
-  store_fields :name, :type
+  store_fields :name, :type, :reporting_date
 
   # This is an array of fields which will uniquely define a record
   # (think primary key in a database)
@@ -23,10 +23,6 @@ class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
   JURISDICTION = "uk"
   URL = "http://foo.com"
 
-  def sample_date
-    Time.now.iso8601(2)
-  end
-
   def jurisdiction_classification
     type
   end
@@ -36,7 +32,7 @@ class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
   # which will validate against a JSON schema.
   def to_pipeline
     {
-      sample_date: sample_date,
+      sample_date: reporting_date,
       company: {
         name: name,
         jurisdiction: JURISDICTION,
@@ -59,13 +55,23 @@ class MyLicence < SimpleOpencBot
   def fetch_records
     # Here we are iterating over an array. Normally you would scrape
     # things from a website and construct LicenceRecords from that.
-    data = [{:name => "foo", :type => "bar"}]
+    data = [
+      {:name => "foo",
+        :type => "bar",
+        :reporting_date => Time.now.iso8601(2)},
+      {:name => "foo 2",
+        :type => "bar",
+        :reporting_date => Time.now.iso8601(2)}
+    ]
+
+    # If you scrape using:
+    client = OpencBot::Client.new
+    response = client.get('http://www.google.co.uk/').body
+    # ... then response will be cached for you automatically for 24
+    # hours, which speeds up debugging quite a lot
 
     data.map do |datum|
-      MyLicenceRecord.new({
-        :name => datum[:name],
-        :type => datum[:type],
-      })
+      MyLicenceRecord.new(datum)
     end
   end
 end
