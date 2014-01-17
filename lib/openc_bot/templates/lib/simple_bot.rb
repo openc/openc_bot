0904d20775d6139c726ee9b656be3cc0dcd23970
Seb Bacon <seb.bacon@gmail.com>
2014-01-17 14:52:54 +0000

    Make the skeleton bot simpler, now we have example bots

--- lib/openc_bot/templates/lib/simple_bot.rb
+++ lib/openc_bot/templates/lib/simple_bot.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 require 'simple_openc_bot'
-require 'mechanize'
 
 # you may need to require other libraries here
 # require 'nokogiri'
+# require 'mechanize'
 
 class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
   # The JSON schema to use to validate records; correspond with files
@@ -18,15 +18,6 @@ class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
   # primary key in a database).
   unique_fields :name
 
-  # These are just example methods and constants used by
-  # `to_pipeline`, below
-  JURISDICTION = "uk"
-  URL = "http://foo.com"
-
-  def jurisdiction_classification
-    type
-  end
-
   # This must be defined, and should return a timestamp in ISO8601
   # format. Its value should change when something about the record
   # has changed. It doesn't have to be a method - it can also be a
@@ -37,18 +28,20 @@ class MyLicenceRecord < SimpleOpencBot::BaseLicenceRecord
 
   # This method must be defined. You can test that you're outputting
   # in the right format with `bin/verify_data`, which will validate
-  # any data you've fetched against the relevant schema.
+  # any data you've fetched against the relevant schema. See
+  # `doc/SCHEMA.md` for documentation.
   def to_pipeline
     {
       sample_date: last_updated_at,
       company: {
         name: name,
-        jurisdiction: JURISDICTION,
+        jurisdiction: "xx",
       },
-      source_url: URL,
+      source_url: "xx",
       data: [{
         data_type: :licence,
         properties: {
+          jurisdiction_code: "xx",
           category: 'Financial',
           jurisdiction_classification: [jurisdiction_classification],
         }
@@ -60,20 +53,15 @@ end
 
 class MyLicence < SimpleOpencBot
 
-  # the class that `fetch_records` yields
+  # the class that `fetch_records` yields. Must be defined.
   yields MyLicenceRecord
 
   # This method should yield Records. It must be defined.
   def fetch_all_records(opts={})
-    agent = Mechanize.new
-    page = agent.get("http://assets.opencorporates.com/test_bot_page.html")
-    doc = Nokogiri::HTML(page.body)
-    doc.xpath("//li").map do |li|
-      name, type = li.content.split(":")
+    data = [{:name => "A", :type => "B"}]
+    data.each do |datum|
       yield MyLicenceRecord.new(
-        :name => name.strip,
-        :type => type.strip,
-        :reporting_date => Time.now.iso8601(2))
+        datum.merge(:reporting_date => Time.now.iso8601(2)))
     end
   end
 end
