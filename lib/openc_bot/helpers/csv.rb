9a3908ed0b6d6f126e320b14a973eeafc7858af3
Countculture <countculture@gmail.com>
2014-01-24 19:30:48 +0000

    namespace stub helpers

--- lib/openc_bot/helpers/csv.rb
+++ lib/openc_bot/helpers/csv.rb
@@ -1,6 +1,8 @@
 module OpencBot
-  module Csv
-    # This module will eventually hold some helper methods for 
-    # dealing with Csv content
+  module Helpers
+    module Csv
+      # This module will eventually hold some helper methods for
+      # dealing with Csv content
+    end
   end
 end
--- lib/openc_bot/helpers/html.rb
+++ lib/openc_bot/helpers/html.rb
@@ -1,6 +1,8 @@
 module OpencBot
-  module Html
-    # This module will eventually hold some helper methods for 
-    # dealing with HTML content
+  module Helpers
+    module Html
+      # This module will eventually hold some helper methods for
+      # dealing with HTML content
+    end
   end
 end
