b2e535439ffefba4eaea2158b6b35bda7b108aff
Countculture <countculture@gmail.com>
2014-03-16 11:06:19 +0000

    Initial very rough commit of company_fetcher_bot

---
+++ lib/openc_bot/templates/lib/company_fetcher_bot.rb
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+require 'company_fetcher_bot'
+
+# you may need to require other libraries here
+#
+# require 'nokogiri'
+# require 'openc_bot/helpers/dates'
+# require 'openc_bot/helpers/incremental_search'
+
+module MyModule
+  extend CompanyFetcherBot
+
+
+end
