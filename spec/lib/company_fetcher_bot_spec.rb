4898f98343e29d564e44603f7f49e463accff01a
Countculture <countculture@gmail.com>
2014-03-17 10:19:56 +0000

    Namespace CompanyFetcherBot

--- spec/lib/company_fetcher_bot_spec.rb
+++ spec/lib/company_fetcher_bot_spec.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
 require_relative '../spec_helper'
 require 'openc_bot'
-require 'company_fetcher_bot'
+require 'openc_bot/company_fetcher_bot'
 
 module TestCompanyFetcherBot
-  extend CompanyFetcherBot
+  extend OpencBot::CompanyFetcherBot
 end
 
 
@@ -21,6 +21,10 @@ describe "A module that extends CompanyFetcherBot" do
 
   it "should include IncrementalHelper methods" do
     TestCompanyFetcherBot.should respond_to(:incremental_search)
+  end
+
+  it "should set PRIMARY_KEY_NAME to :company_number" do
+    TestCompanyFetcherBot.primary_key_name.should == :company_number
   end
 
   describe "#validate_datum" do
