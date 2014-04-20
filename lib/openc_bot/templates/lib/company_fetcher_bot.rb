c3ff1f1045c7316dbfd2e4f443159dd2ebbb65f5
Countculture <countculture@gmail.com>
2014-04-20 18:00:00 +0100

    Update company_bot documentation

--- lib/openc_bot/templates/lib/company_fetcher_bot.rb
+++ lib/openc_bot/templates/lib/company_fetcher_bot.rb
@@ -5,13 +5,25 @@ require 'openc_bot/company_fetcher_bot'
 # you may need to require other libraries here
 #
 # require 'nokogiri'
+
+# uncomment (and line further down) to get Date helper methods. (Also available csv and text helpers)
 # require 'openc_bot/helpers/dates'
 
 module MyModule
   extend OpencBot
   # This adds the CompanyFetcherBot functionality
   extend OpencBot::CompanyFetcherBot
+  # uncomment to get Date helper methods
+  # extend OpencBot::Helpers::Dates
   extend self # make these methods as Module methods, rather than instance ones
+
+
+  # Uncomment to use alpha search – default is incremental search
+  # USE_ALPHA_SEARCH = true
+
+  # Default number of characters used for search terms in alpha search. Default is 1 (i.e. 'A','B'...)
+  # NUMBER_OF_CHARS_IN_SEARCH = 3
+
 
   # If the register has a GET'able URL based on the company_number define it here. This should mean that
   # #fetch_datum 'just works'.
@@ -20,14 +32,15 @@ module MyModule
     # "http://some,register.com/path/to/#{company_number}"
   end
 
-  # This is the primary method for getting companies from the register. By default it uses the #fetch_data
-  # method defined in IncrementalSearch helper module, which increments through :company_number identifiers.
-  # See helpers/incremental_search.rb for details
-  # Override this if a different method for iterating companies is going to done (e.g. an alpha search, or
+  # #fetch_data is the primary method for getting companies from the register, and by default is
+  # called when the bot is 'run' (e.g. via bundle exec openc_bot rake bot:run, which calls
+  # #update_data, which in turn calls this)
+  # By default this uses an incremental search (which increments through :company_number identifiers),
+  # or if USE_ALPHA_SEARCH has been set, an alpha search (e.g. searching for entities using 'AA', 'AB')
+  # Define this locally this if a different method for getting companies is going to done (e.g.
   # parsing a CSV file)
-  def fetch_data
-    fetch_data_via_incremental_search
-  end
+  # def fetch_data
+  # end
 
   # This is called by #update_datum (defined in the IncrementalSearch helper module), which updates the
   # information for a given company_number. This allows the individual records to be updated, for example,
@@ -41,14 +54,23 @@ module MyModule
   # converted to json, and stored in the database in the row for that company number, under the :data key,
   # so that it can be reused or referred it in the future.
   # {:company_page => company_page_html, :filings_page => filings_page_html}
-  def fetch_datum(company_number)
-    super
+  # def fetch_datum(company_number)
+  # end
+
+  # This method must be defined for all bots that can fetch and process individual records, e.g.
+  # incremental searchers, or individual company pages in an alpha search.
+  # Where the bot cannot do this (e.g. where the underlying data is
+  # only available as a CSV file, or there are no individual pages for each company, it can be
+  # left as a stub method)
+  def process_datum(datum_hash)
+    # write your code to parse what is in the company pages/data
   end
 
-  # This method must be defined for all bots that can fetch and process individual records, including
-  # incremental, and alpha searchers. Where the bot cannot do this (e.g. where the underlying data is
-  # only available as a CSV file, it can be left as a stub method)
-  def process_datum(datum_hash)
+  # This method is called by #fetch_data_via_alpha_search (defined in AlphaSearch helper),
+  # and is passed a search term, typically search term of a number of characters (e.g. 'AB', 'AC'...).
+  # This method should yield a hash of company data which can be validated to the company-schema
+  def search_for_entities_for_term(term, options={})
+    # write your code to search all the pages for the given term, and yield a series of company hashes
   end
 
 end
