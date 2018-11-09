4ca70fb4262f01b488c1686e70992464795ab214
Dominic Sayers <dominic@sayers.cc>
2018-11-09 10:53:34 +0000

    Fix hash formatting
    Currently layout is confusing to `rubocop --auto-correct`

--- spec/lib/bot_data_validator_spec.rb
+++ spec/lib/bot_data_validator_spec.rb
@@ -6,20 +6,25 @@ describe OpencBot::BotDataValidator do
 
   describe '#validate' do
     before do
-      @valid_data =
-        { :company => {:name => "CENTRAL BANK", :identifier => "rssd/546544", :jurisdiction => "IA"},
-          :data => [{ :data_type => :subsidiary_relationship,
-                      :properties => {:foo => 'bar'}
-                      },
-                    { :data_type => :subsidiary_relationship,
-                      :properties => { :foo => 'baz' }
-                    }
-                      ],
-          :source_url => "http://www.ffiec.gov/nicpubweb/nicweb/OrgHierarchySearchForm.aspx?parID_RSSD=546544&parDT_END=99991231",
-          :reporting_date => "2013-01-18 12:52:20"
-        }
-
-
+      @valid_data = {
+        :company => {
+          :name => "CENTRAL BANK",
+          :identifier => "rssd/546544",
+          :jurisdiction => "IA"
+        },
+        :data => [
+          {
+            :data_type => :subsidiary_relationship,
+            :properties => {:foo => 'bar'}
+          },
+          {
+            :data_type => :subsidiary_relationship,
+            :properties => { :foo => 'baz' }
+          }
+        ],
+        :source_url => "http://www.ffiec.gov/nicpubweb/nicweb/OrgHierarchySearchForm.aspx?parID_RSSD=546544&parDT_END=99991231",
+        :reporting_date => "2013-01-18 12:52:20"
+      }
     end
     it 'should return true if data is valid' do
       expect(OpencBot::BotDataValidator.validate(@valid_data)).to be true
