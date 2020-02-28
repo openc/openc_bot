23f168310a45fbeed79adf5bc2080367072ada95
Harry Wood <harry.wood@opencorporates.com>
2020-02-28 15:42:29 +0000

    Eliminate require_relative in helper specs
    After moving the files these `require_relative`s are broken, but this is bad practice anyway. Use straight `require`s instead.

--- spec/lib/openc_bot/helpers/register_methods_spec.rb
+++ spec/lib/openc_bot/helpers/register_methods_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../../spec_helper"
+require "spec_helper"
 require "openc_bot"
 require "openc_bot/helpers/register_methods"
 
--- spec/lib/openc_bot/helpers/text_spec.rb
+++ spec/lib/openc_bot/helpers/text_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../../spec_helper"
+require "spec_helper"
 require "openc_bot/helpers/text"
 
 describe OpencBot::Helpers::Text do
