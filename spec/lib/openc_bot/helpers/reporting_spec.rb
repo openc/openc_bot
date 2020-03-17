openc/openc_bot

commit 3d58bafe5a6f250ca7a15310c5218eb22d617e1d

Provide subject_details option on send_error_report
Provide a new `:subject_details` option on the `send_error_report` method, allowing a bot developer to override the default (simple exception message) output.

This will allow us (as and when we are ready to do so), to blow away of a lot of messy code in our bots, where we override (and largely duplicate) the send_error_report method.

lib/openc_bot/helpers/reporting.rb
@@ -27,8 +27,9 @@ module OpencBot
       end
 
       def send_error_report(exception, options = {})
-        subject = "Error running #{name}: #{exception}"
-        body = "Error details: #{exception.inspect}.\nBacktrace:\n#{exception.backtrace}"
+        subject_details = options[:subject_details] || exception
+        subject = "Error running #{name}: #{subject_details}"
+        body = "Error details: #{e.inspect}.\nBacktrace:\n#{exception.backtrace}"
         send_report(subject: subject, body: body)
         report_run_to_analysis_app(output: body, status_code: "0", ended_at: Time.now.to_s, started_at: options[:started_at])
       end

commit c14af8eadaac8c929a39e274838fda59c14a73e6

Fix misnamed variable bug
Fix a bug introduced in 3d58bafe5a6f250ca7a15310c5218eb22d617e1d . `exception` accidentally renamed `e`.

Add test coverage!

lib/openc_bot/helpers/reporting.rb
@@ -29,7 +29,7 @@ module OpencBot
       def send_error_report(exception, options = {})
         subject_details = options[:subject_details] || exception
         subject = "Error running #{name}: #{subject_details}"
-        body = "Error details: #{e.inspect}.\nBacktrace:\n#{exception.backtrace}"
+        body = "Error details: #{exception.inspect}.\nBacktrace:\n#{exception.backtrace}"
         send_report(subject: subject, body: body)
         report_run_to_analysis_app(output: body, status_code: "0", ended_at: Time.now.to_s, started_at: options[:started_at])
       end

spec/lib/openc_bot/helpers/reporting_spec.rb
@@ -10,6 +10,40 @@ module ModuleThatIncludesReporting
 end
 
 describe OpencBot::Helpers::Reporting do
+  before do
+    allow(ModuleThatIncludesReporting).to receive(:reporting_enabled?).and_return(true)
+    allow(ModuleThatIncludesReporting).to receive(:_analysis_http_post)
+  end
+
+  describe "#send_error_report" do
+    context "without parameters" do
+      before do
+        allow(ModuleThatIncludesReporting).to receive(:send_report)
+        allow(ModuleThatIncludesReporting).to receive(:report_run_to_analysis_app)
+        exception = RuntimeError.new("something went wrong")
+        ModuleThatIncludesReporting.send_error_report(exception)
+      end
+
+      it "calls send_report with suitable subject and body" do
+        expect(ModuleThatIncludesReporting).to have_received(:send_report)
+          .with(
+            body: "Error details: #<RuntimeError: something went wrong>.\nBacktrace:\n",
+            subject: "Error running ModuleThatIncludesReporting: something went wrong",
+          )
+      end
+
+      it "calls report_run_to_analysis_app with '0' status and body details" do
+        expect(ModuleThatIncludesReporting).to have_received(:report_run_to_analysis_app)
+          .with(
+            status_code: "0",
+            output: "Error details: #<RuntimeError: something went wrong>.\nBacktrace:\n",
+            started_at: nil,
+            ended_at: kind_of(String),
+          )
+      end
+    end
+  end
+
   describe "#report_run_progress" do
     it "posts a progress report to the analysis app fetcher_progress_log endpoint" do
       expected_params = { data: { bot_id: "module_that_includes_reporting", companies_processed: 3, companies_added: 2, companies_updated: 1 }.to_json }