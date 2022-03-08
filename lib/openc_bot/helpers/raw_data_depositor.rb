fc693d2c13f86aa248b340440d2849dafde09e66
Liz Conlan <liz.conlan@opencorporates.com>
2022-03-08 18:11:32 +0000

    Use postbox_client gem for bot incision
    Swaps out rest_client for a pre-release postbox_client gem and updates
    the send_raw_datum method accordingly
    
    Uploads to the following path structure in S3:
    
    us_wa_companies_fetcher/2022-03-10T13:04:49Z-4d84294ef9/1646917503/filings_0.json
    
    Made up of: bot_name / run_id (as a UTC datetime stamp) + '-' + git_sha / retrieved_at (as an intger (Unix epoch)) / filename
    
    (Although the retrieved_at could easily be a sequential integer, it just needs to be unique within the run)

--- lib/openc_bot/helpers/raw_data_depositor.rb
+++ lib/openc_bot/helpers/raw_data_depositor.rb
@@ -1,15 +1,9 @@
 require 'openc_bot/bot_config'
-require 'rest_client'
+require 'postbox_client'
 
 module OpencBot
   module Helpers
     class RawDataDepositor
-
-      BASE_URI = "http://Default-External-1855876749.eu-west-1.elb.amazonaws.com:2001/datum/"
-      HEADERS = {
-        'X-Hacky-Access-Token' => '4cbc7d3b-fc44-42e6-8ffc-4ab109e945b9'
-      }
-
       def self.deposit_raw_datum(raw_datum_hash)
         depositor = new(raw_datum_hash)
         depositor.deposit_raw_datum
@@ -30,27 +24,31 @@ module OpencBot
       private
 
       def send_raw_datum
-        endpoint = URI.join(BASE_URI, raw_datum_id).to_s
-        request = RestClient::Request.new(
-          :method => :post,
-          :url => endpoint,
-          :headers => HEADERS,
-          :payload => {
-            :multipart => true,
-            "data" => @raw_datum_files.values.map { |f| File.open(f) }
-          })
-        request.execute
+        postbox_client.upload_datum_files(
+          @raw_datum_files.values.map { |f| File.new(f) },
+          OpencBot::BotConfig.instance.bot_id,
+          Time.at(OpencBot::BotConfig.instance.run_id).utc.iso8601.sub(/\+00:00$/, "Z"),
+          OpencBot::BotConfig.instance.git_sha,
+          @retrieved_at
+        )
+      end
+
+      def postbox_client
+        @postbox_client ||= connect_to_postbox
+      end
+
+      def connect_to_postbox
+        # server_url = "http://Default-External-1855876749.eu-west-1.elb.amazonaws.com:2001"
+        # server_url = "http://a54dbfc3eaefd412b9f6982d7fd2adca-1931796360.eu-west-2.elb.amazonaws.com"
+        server_url = "http://localhost:8000"
+
+        api_instance = PostboxClient::DefaultApi.new(server_url)
       end
 
       def delete_raw_datum_file_references
         @raw_datum_files.values.each do |file_location|
           FileUtils.rm(file_location)
         end
-      end
-
-      def raw_datum_id
-        @raw_datum_id ||= "#{OpencBot::BotConfig.instance.bot_id}--#{OpencBot::BotConfig.instance.run_id}--"\
-                          "#{OpencBot::BotConfig.instance.git_sha}--#{@retrieved_at}"
       end
     end
   end
